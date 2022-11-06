5709c15736cef785a1016fe2b93a769f5621d689
chendanjun <cdj_2016_bj@icloud.com>
2022-11-06 11:42:59 +0800

    MOD: 关于页

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -17,12 +17,26 @@
         </div>
       </div>
     </div>
+    <div class="about-introduction">
+      <img :src="introductionImg" />
+      <div class="content">
+        <h2>苏州凌光自动化科技有限公司</h2>
+        <div>
+          苏州凌光自动化科技有限公司成立于2018年，坐落于美丽的苏州工业园区，是一家专业提供图尔克传感器、伺服压机、RFID电子标签等产品的一站式采购和技术支持的供应商。
+          <br />
+          公司的优势在于“货源直接，库存充足，价格合理”，并向广大客户承诺只提供全新原装正品。产品应用领域涵盖：电源管理，工业通信，安防，消防火灾，仪器仪表，医疗电源，工业控制器，电力自动化及汽车电子等领域。
+          <br/>
+          公司秉承“诚信，专业，持续发展”的经营理念和“一流品质，一流服务”的发展方针！赢得了广大客户的高度信任和大力支持!
+        </div>
+      </div>
+    </div>
     <Footer />
   </div>
 </template>
 
 <script>
 import ImgHeader from '../components/ImgHeader.vue';
+import IntroductionImg from '@/assets/about-us';
 import Footer from '../components/Footer.vue';
 
 export default {
@@ -38,6 +52,7 @@ export default {
         { value: 'partner', label: '合作伙伴'},
       ],
       currentPosition: '',
+      introductionImg: IntroductionImg,
     }
   },
   watch: {
